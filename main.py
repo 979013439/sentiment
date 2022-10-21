cf8d48ff7064f01ec2a7dac7c7a966a380a49a51
leihuang <leihuang007@qq.com>
2022-10-21 18:04:56 +0800

    增加语料库清洗代码。

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, f1_score, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import numpy as np
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 import pickle
 
+import re
+import math
+
 np.random.seed(1)
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score, f1_score, confusion_matrix
-from sklearn.model_selection import train_test_split
 
 df_ = pd.read_csv('facebook_sentiment.csv')
-df_.head()
+
 df = df_[['sentimental', 'text']].copy()
-df.head()
-df['sentimental'].hist()
+# df['sentimental'].hist()
 target_map = {'positive': 1, 'negative': 0, 'neutral': 2}
 df['target'] = df['sentimental'].map(target_map)
-df.head()
+
+# 清洗语料库
+for i in range(0, df.shape[0]):
+    _text = df['text'][i]
+    _result: float = df['target'][i]
+    if (type(_text) == float or math.isnan(_result)):
+        df.drop(i, inplace=True)
+        continue
+    df['text'][i] = re.sub('[^a-zA-Z]', ' ', df['text'][i])
+
+
 df_train, df_test, = train_test_split(df)
-df_train.head()
+
 vectorizer = TfidfVectorizer(max_features=2000)
 X_train = vectorizer.fit_transform(df_train['text'])
 X_test = vectorizer.transform(df_test['text'])
@@ -38,15 +50,18 @@ Pr_test = model.predict_proba(X_test)
 print("Train AUC: ", roc_auc_score(Y_train, Pr_train, multi_class='ovo'))
 print("Test  AUC: ", roc_auc_score(Y_test, Pr_test, multi_class='ovo'))
 P_train = model.predict(X_train)
-P_test  = model.predict(X_test)
+P_test = model.predict(X_test)
 cm = confusion_matrix(Y_train, P_train, normalize='true')
 cm
+
+
 def plot_cm(cm):
     classes = ['negative', 'positive', 'neutral']
     df_cm = pd.DataFrame(cm, index=classes, columns=classes)
     ax = sn.heatmap(df_cm, annot=True, fmt='g')
     ax.set_xlabel('Predicted')
     ax.set_ylabel("Target")
+
 
 plot_cm(cm)
 cm_test = confusion_matrix(Y_test, P_test, normalize='true')
