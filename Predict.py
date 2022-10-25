e07ad9823436849f75045a5aec27bc4580bed24b
979013439 <116045724+979013439@users.noreply.github.com>
2022-10-25 21:00:04 +0800

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -2,8 +2,11 @@ import pickle
 import pandas as pd
 vectorizer = pickle.load(open('./sentiment-analysis-lr-vocabulary.pkl', 'rb'))
 model      = pickle.load(open('./sentiment-analysis-lr-model.pkl', 'rb'))
-sentence = "he do not like this phone"
-model.predict(vectorizer.transform([sentence]))
-
-
-
+sentence = ["he do not like this phone"]
+res=model.predict(vectorizer.transform(sentence))
+if(res[0]==0):
+    print('Negatvie')
+elif(res[0]==1):
+    print('Positive')
+else:
+    print('Neutral')
